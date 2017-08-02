04fd1233bdc02672d1edd552662a0753752db03d
ens-carlos <carlos@ebi.ac.uk>
2017-08-02 15:23:09 +0100

    Some environment variables.

---
+++ scripts/env.sh
@@ -0,0 +1,5 @@
+export ENSCODE=/path/to/ensembl/code
+export PERL5LIB=$ENSCODE/ensembl/modules:$BIOPERL_LIB:$ENSCODE/ensembl-analysis/modules:$ENSCODE/GIFTS/modules:/path/to/Bio-DB-HTS/lib
+export LOGS=/path/to/logs
+export UNIPROT_DIR=/path/to/data/uniprot_20YY_MM
+
