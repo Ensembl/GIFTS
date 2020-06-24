b83aaf1ef89632a31e7e0119139810ff2bb2f456
ens-carlos <ens-carlos@users.noreply.github.com>
2020-06-24 12:20:56 +0100

    Make sure the output directory exists beforing preparing the Uniprot files.

--- modules/Bio/EnsEMBL/GIFTS/PipeConfig/HiveLoadGIFTS_alignments_conf.pm
+++ modules/Bio/EnsEMBL/GIFTS/PipeConfig/HiveLoadGIFTS_alignments_conf.pm
@@ -112,7 +112,7 @@ sub pipeline_analyses {
       -parameters => {
                         use_bash_pipefail => 1, # Boolean. When true, the command will be run with "bash -o pipefail -c $cmd". Useful to capture errors in a command that contains pipes
                         use_bash_errexit  => 1, # When the command is composed of multiple commands (concatenated with a semi-colon), use "bash -o errexit" so that a failure will interrupt the whole script
-                        cmd => 'sh '.$self->o('prepare_uniprot_script').' '.$self->o('uniprot_dir').' #output_dir#'
+                        cmd => 'mkdir -p #output_dir# ; sh '.$self->o('prepare_uniprot_script').' '.$self->o('uniprot_dir').' #output_dir#'
                      },
       -rc_name   => 'default',
       -flow_into => { 1 => ['prepare_uniprot_files_indexes'] },
